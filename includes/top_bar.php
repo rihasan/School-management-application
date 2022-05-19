52d624eb09edc474f5c20c6180fe0bc8e603cdf8
A. H. M Riazul Islam <rihasan@users.noreply.github.com>
2022-05-20 02:17:17 +0600

    version 1.0

---
+++ includes/top_bar.php
@@ -0,0 +1,38 @@
+<div class="ed-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="ed-com-t1-left">
+                    <ul>
+                        <li><a href="#">Contact: Room No 401, Monjuri Bhaban, 8-DIT Avem Motijhel, Dhaka.</a>
+                        </li>
+                        <li><a href="#">Phone: +880168037570</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="ed-com-t1-right">
+                    <ul>
+                        <li><a href="admin/" id="top_singin">Admin</a>
+                        </li>
+                        <li><a href="std-login.php" id="top_singin">Student Sign In</a>
+                        </li>
+                        <li><a href="std_signup.php" id="t">Student Sign Up</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="ed-com-t1-social">
+                    <ul>
+                        <li><a href="https://www.facebook.com/yearulislamonem" target="_blank"><i class="fa fa-facebook"
+                                    aria-hidden="true"></i></a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                        </li>
+                        <li><a href="https://twitter.com/PcYearul" target="_blank"><i class="fa fa-twitter"
+                                    aria-hidden="true"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
